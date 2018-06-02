3b7d9daeadd413e85a83f060dfb777fb590534b3
thoffart <thiagohoffart321920@gmail.com>
2018-06-01 21:41:48 -0300

    grafico adiconado com plugin para mostrar os dados direto nele

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,24 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ViewChild, OnInit, ElementRef } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { Chart } from "chart.js";
+import "chartjs-plugin-datalabels";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/fromEvent";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/switch";
+import { from } from "rxjs/observable/from";
+import { map } from "rxjs/operators";
+import { mapTo } from "rxjs/operators";
 
 @IonicPage()
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
-export class HomePage {
+export class HomePage implements OnInit {
   categorias = [
     "Alimentação",
     "Animal de estimação",
@@ -24,10 +35,21 @@ export class HomePage {
   ];
   @ViewChild("doughnutCanvas") doughnutCanvas;
   doughnutChart: any;
+  //search = ElementRef("search");
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
+  ngOnInit() {
+    /* Observable.fromEvent(this.search.nativeElement, "keyup")
+      .map((e: any) => e.target.value)
+      .filter((text: string) => this.expect(text))
+      .switch()
+      .subscribe((text: string) => {
+        //this.data = this.calcnotas(text)
+      }); */
+  }
+
   ionViewDidLoad() {
-    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+    /* this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
       type: "doughnut",
       data: {
         labels: ["Gastos", "Receita"],
@@ -44,12 +66,74 @@ export class HomePage {
         ]
       },
       options: {
+        plugins: {
+          datalabels: {
+            backgroundColor: function(context) {
+              return context.dataset.backgroundColor;
+            },
+            borderColor: "white",
+            borderRadius: 25,
+            borderWidth: 2,
+            color: "white",
+            display: function(context) {
+              var dataset = context.dataset;
+              var count = dataset.data.length;
+              var value = dataset.data[context.dataIndex];
+              return value > count * 1.5;
+            },
+            font: {
+              weight: "bold"
+            },
+            formatter: Math.round
+          }
+        }
+      }
+    });
+ */
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: "doughnut",
+      data: {
+        labels: ["Gastos", "Receita"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [3569, 2290],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)"
+            ],
+            hoverBackgroundColor: ["#FF6384", "#36A2EB"]
+          }
+        ]
+      },
+      options: {
         legend: {
           display: true,
           position: "bottom",
           labels: {
             boxWidth: 50,
             fontColor: "black"
+          }
+        },
+        plugins: {
+          datalabels: {
+            backgroundColor: function(context) {
+              return context.dataset.backgroundColor;
+            },
+            borderColor: "white",
+            borderRadius: 25,
+            borderWidth: 2,
+            color: "white",
+            display: function(context) {
+              var dataset = context.dataset;
+              var count = dataset.data.length;
+              var value = dataset.data[context.dataIndex];
+              return value > count * 1.5;
+            },
+            font: {
+              weight: "bold"
+            },
+            formatter: Math.round
           }
         }
       }
